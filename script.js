4f4f4421f6b108e9869f89025a16fae1d6c1ec3e
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-04-26 21:14:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-// complete the given function
-
 function palindrome(str){
-
+	
+// Step 1. The first part is the same as earlier
  var re = /[^A-Za-z0-9]/g;
  str = str.toLowerCase().replace(re, '');
+	
+	 // Step 2. Create the FOR loop
  var len = str.length;
  for (var i = 0; i < len/2; i++) {
    if (str[i] !== str[len - 1 - i]) {
