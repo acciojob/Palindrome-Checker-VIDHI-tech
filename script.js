424e300eeb66116c1e791b9def998100308ff9ea
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-04-26 20:59:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // complete the given function
 
-function palindrome(str){
-
+function palindrome(str) {
+  var re = /[\W_]/g;
+  var lowRegStr = str.toLowerCase().replace(re, '');
+  var reverseStr = lowRegStr.split('').reverse().join(''); 
+  return reverseStr === lowRegStr;
+}
+palindrome("A man, a plan, a canal. Panama");
 }
 module.exports = palindrome
