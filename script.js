d9dca2ad899fc87f35845179be47d95de736e684
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-04-26 21:01:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 // complete the given function
 
-function palindrome(str) {
-  var re = /[\W_]/g;
-  var lowRegStr = str.toLowerCase().replace(re, '');
-  var reverseStr = lowRegStr.split('').reverse().join(''); 
-  return reverseStr === lowRegStr;
-}
-palindrome("A man, a plan, a canal. Panama");
+function palindrome(str){
+
+ var re = /[^A-Za-z0-9]/g;
+ str = str.toLowerCase().replace(re, '');
+ var len = str.length;
+ for (var i = 0; i < len/2; i++) {
+   if (str[i] !== str[len - 1 - i]) {
+       return false;
+   }
+ }
+ return true;
 }
 module.exports = palindrome
